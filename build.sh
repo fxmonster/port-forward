90b56051e3110d5b65aa32cd642f92f50880fbf2
Anton Bakaev <fxmonster@gmail.com>
2021-07-11 15:17:45 +0300

    add udp listen

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 docker build ./udp/Dockerfile -t fxmonster/port-forward-udp:latest
 docker build ./tcp/Dockerfile -t fxmonster/port-forward-tcp:latest
-
 docker build . -t fxmonster/port-forward:latest
+
+docker push fxmonster/port-forward:latest
+docker push fxmonster/port-forward-tcp:latest
+docker push fxmonster/port-forward-udp:latest
